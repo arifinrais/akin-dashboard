839b6cf16bc046c5efea2f889c2599a440082b8c
Arifin Rais <44962398+arifinrais@users.noreply.github.com>
2021-04-01 13:56:48 +0700

    [DEV] to MERN

--- client/index.js
+++ client/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './css/index.css';
 //import App from './App';
 import Tes from './tes';
-import reportWebVitals from './reportWebVitals';
 
 
 /*ReactDOM.render(
@@ -23,4 +22,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+//webpack.config.js
+var path = require('path');
+var webpack = require('webpack');
+module.exports = {
+ entry: './client/index.js',
+ output: {
+  path: path.join(__dirname, 'client'),
+  filename: 'bundle.js'
+ },
+ module: {
+  rules: [{
+   test: /.jsx?$/,
+   loader: 'babel-loader',
+   exclude: /node_modules/,
+   query: {
+    presets: ['@babel/preset-env', '@babel/react']
+   }
+  },
+  {
+   test: /\.css$/,
+   loader: "style-loader!css-loader"
+  },
+  {
+    test: /\.svg$/,
+    use: [
+      {
+        loader: 'svg-url-loader',
+        options: {
+          limit: 10000,
+        },
+      },
+    ],
+  }
+  ]
+ }
+}
