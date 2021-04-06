80b95226e6af154ede8f945eb52a98072fd999be
Arifin Rais <44962398+arifinrais@users.noreply.github.com>
2021-04-06 07:55:19 +0700

    [DEV] check another rendering of DOM

--- client/components/App.js
+++ client/components/App.js
@@ -1,5 +1,5 @@
 //import logo from './logo.svg';
-import './css/App.css';
+import '../css/App.css';
 
 function App() {
   return (
--- client/index.js
+++ client/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
-//import App from './App';
+import App from './components/App';
 import Tes from './components/tes';
 
 
-/*ReactDOM.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);*/
+);
 
 ReactDOM.render(
   <React.StrictMode>
