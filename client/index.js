c0f4d39d95397cd12a45a00f0f848b902df1c95a
Arifin Rais <44962398+arifinrais@users.noreply.github.com>
2021-06-08 10:45:56 +0700

    [TEST] React Components

--- client/index.js
+++ client/index.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './components/App';
 import Tes from './components/tes';
+import AkinNavbar from './components/AkinNavbar';
 
+ReactDOM.render(
+  <React.StrictMode>
+    <AkinNavbar />
+  </React.StrictMode>,
+  document.getElementById('navbar')
+);
 
 ReactDOM.render(
   <React.StrictMode>
