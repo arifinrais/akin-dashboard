a3b55fee52476fcccd0bfe930b297cface957373
Arifin Rais <44962398+arifinrais@users.noreply.github.com>
2021-06-08 16:07:04 +0700

    [DEV] Navbar Right

--- client/components/AkinNavbar.js
+++ client/components/AkinNavbar.js
@@ -9,13 +9,13 @@ const AkinNavbar = props => {
                 <img src="/res/logowarnagelap.png" width="84.88" height="45" alt="" />
             </Nav.Link>
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
+                <Nav className="ml-auto">
                     <Nav.Link href="/explore">JELAJAHI</Nav.Link>
-                    <NavDropdown title="DAERAH" id="basic-nav-dropdown">
+                    <NavDropdown alignRight title="DAERAH" id="basic-nav-dropdown">
                         <NavDropdown.Item href="/region">Profil Daerah</NavDropdown.Item>
                         <NavDropdown.Item href="/ranking">Peringkat Daerah</NavDropdown.Item>
                     </NavDropdown>
-                    <NavDropdown title="PELAJARI" id="basic-nav-dropdown">
+                    <NavDropdown alignRight title="PELAJARI" id="basic-nav-dropdown">
                         <NavDropdown.Item href="/learn#data">Pengolahan Data</NavDropdown.Item>
                         <NavDropdown.Item href="/learn#rca">RCA</NavDropdown.Item>
                         <NavDropdown.Item href="/learn#network">Jejaring Bimodal</NavDropdown.Item>
