95478edf35b8ff9af1bf6b76e5f554997d613620
Arifin Rais <44962398+arifinrais@users.noreply.github.com>
2021-06-11 14:43:54 +0700

    [DEV] coba via routing
    BISA JADI YANG POLYFILL GAPERLU, gabisa via client ngakses mongodbnya

--- client/components/AkinViz.js
+++ client/components/AkinViz.js
@@ -2,21 +2,43 @@ import React, {Component} from 'react';
 import {DataViz, VizType} from 'react-fast-charts';
 import * as d3 from 'd3';
 
-const MongoClient = new require('mongodb').MongoClient;
+/*const MongoClient = new require('mongodb').MongoClient;
 const client = new MongoClient('mongodb://mongo:admin@localhost:27017', { useUnifiedTopology: true, useNewUrlParser: true });
 console.log("tes1")
 
 async function run() {
   try {
-    await client.connect();
     console.log("tes2")
+    await client.connect();
+    const coll = client.db('akin-dashboard').collection('viz_patent')
+    const cursor = coll.find({year:2001})
+    await cursor.forEach(console.dir);
+    console.log("tes2b")
   } finally {
     await client.close();
     console.log("tes3")
   }
 }
 
-run().catch(console.dir);
+
+run().catch(console.dir);*/
+
+//gagal di connect karena i.unescape is not a function, katanya mongodb gabisa diakses dari browser?
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://mongo:admin@localhost:27017";
+console.log("tes")
+MongoClient.connect(url, { useUnifiedTopology: true, useNewUrlParser: true }, (err, db) => {
+  console.log("tes1")
+  if (err) throw err;
+  var dbo = db.db("akin-dashboard");
+  var query = { year: 2018 };
+  dbo.collection("viz_patent").find(query).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result["total_national"]["A"]);
+    db.close();
+  });
+});
+
 
 const RootDatum = {
     "id": "example-tree-map-data",
