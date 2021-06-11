5bcf3305f771aa1f7a1bef1a651aa2d956064adc
Arifin Rais <44962398+arifinrais@users.noreply.github.com>
2021-06-11 09:26:07 +0700

    [DEV] try to implement explore route

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,7 +1,18 @@
 //server/routes/routes.js
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+
+//homepage
 router.get('/', function(req, res){
   res.render('index')
 });
+
+//explore
+router.get('/explore', function(req) {
+  
+});
+
+
+
 module.exports = router;
