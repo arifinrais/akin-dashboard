181cd58f8cec5c019810824d9671021dc31846b9
Arifin Rais <44962398+arifinrais@users.noreply.github.com>
2021-06-12 17:00:08 +0700

    [REVAMP] dirapihin lagi

--- server/providers/modelProvider.js
+++ server/providers/modelProvider.js
@@ -1,8 +1,8 @@
 const main = {};
 
-main.Patent = require("../models/Patent");
-main.PatentNSV = require("../models/PatentNSV");
-main.PatentOTV = require("../models/PatentOTV");
-main.PatentTMV = require("../models/PatentTMV");
+main.Patent = require("../models/documents/Patent");
+main.PatentNSV = require("../models/visualizations/Nationalshare");
+main.PatentOTV = require("../models/visualizations/Overtime");
+main.PatentTMV = require("../models/visualizations/Treemap");
 
 module.exports = main;
