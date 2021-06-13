78d490b6f34e297b35d991f00e2d9cc55f108346
Arifin Rais <44962398+arifinrais@users.noreply.github.com>
2021-06-13 13:04:47 +0700

    [DEV] Try Mongoose

--- server/server.js
+++ server/server.js
@@ -3,14 +3,17 @@ var express = require('express');
 var router = require('./routes/routes.js')
 var path = require('path');
 var app = express();
-var url = "mongodb://localhost:27017/";
-var bodyParser = require('body-parser');
+//var url = "mongodb://localhost:27017/";
+var mongoose = require('mongoose')
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../client'));
 app.use(express.static(path.join(__dirname, '../client')));
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: false}));
+
+mongoose.connect('mongodb://mongo:admin@localhost:27017');
+
 app.use('/', router);
 
 module.exports=app;
