cab8b1c31fda3937d38b1d70aa1a029188009df0
Arifin Rais <44962398+arifinrais@users.noreply.github.com>
2021-06-15 23:38:15 +0700

    [DEV] Slider done

--- client/components/dashboard/Slider.js
+++ client/components/dashboard/Slider.js
@@ -7,7 +7,7 @@ import Tooltip from "@material-ui/core/Tooltip";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    width: 600 + theme.spacing(3) * 2
+    width: '100%'
   },
   margin: {
     height: theme.spacing(3)
@@ -18,7 +18,7 @@ function ValueLabelComponent(props) {
   const { children, open, value } = props;
 
   return (
-    <Tooltip open={open} enterTouchDelay={0} placement="top" title={value}>
+    <Tooltip arrow open={open} enterTouchDelay={0} placement="top" title={value}>
       {children}
     </Tooltip>
   );
@@ -30,9 +30,7 @@ ValueLabelComponent.propTypes = {
   value: PropTypes.number.isRequired
 };
 
-const iOSBoxShadow =
-  "0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.13),0 0 0 1px rgba(0,0,0,0.02)";
-
+//tar ini diisi sama state pake component did mount juga, parent-child harus dibenerin
 const marks = [{
     value: 2000,
     label: '2000'
@@ -101,102 +99,74 @@ const marks = [{
     label: '2018'
   }];
 
-const IOSSlider = withStyles({
-  root: {
-    color: "#dcdcdc",
-    height: 2,
-    padding: "15px 0"
-  },
-  thumb: {
-    height: 28,
-    width: 28,
-    backgroundColor: "#fff",
-    boxShadow: iOSBoxShadow,
-    marginTop: -14,
-    marginLeft: -14,
-    "&:focus, &:hover, &$active": {
-      boxShadow:
-        "0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.3),0 0 0 1px rgba(0,0,0,0.02)",
-      // Reset on touch devices, it doesn't add specificity
-      "@media (hover: none)": {
-        boxShadow: iOSBoxShadow
+const TimelineSlider = withStyles({
+    root: {
+      color: "#dcdcdc",
+      height: 3,
+      padding: "8px 0"
+    },
+    thumb: {
+      height: 24,
+      width: 0.1,
+      backgroundColor: "#717171",
+      border: "1px solid #717171",
+      marginTop: -9,
+      marginLeft: 0,
+      boxShadow: "#ebebeb 0 2px 2px",
+      "&:focus, &:hover, &$active": {
+        boxShadow: "#ccc 0 2px 3px 1px"
+      },
+      "& .bar": {
+        // display: inline-block !important;
+        height: 9,
+        width: 1,
+        backgroundColor: "currentColor",
+        marginLeft: 1,
+        marginRight: 1
       }
+    },
+    active: {},
+    valueLabel: {
+      left: "calc(+50% - 10px)",
+      top: -16,
+      "& *": {
+        background: "transparent",
+        color: "#fff",
+        backgroundColor: "#000"
+      }
+    },
+    track: {
+      height: 8,
+      opacity: 1,
+      borderRadius: 5,
+    },
+    rail: {
+      height:8,
+      opacity: 1,
+      borderRadius: 5
+    },
+    mark: {
+      backgroundColor: "#bfbfbf",
+      height: 8,
+      width: 1,
+      marginTop: 0,
+      color: "bfbfbf"
     }
-  },
-  active: {},
-  valueLabel: {
-    left: "calc(-50% + 12px)",
-    top: -22,
-    "& *": {
-      background: "transparent",
-      color: "#000"
-    }
-  },
-  track: {
-    height: 2
-  },
-  rail: {
-    height: 5,
-    opacity: 0.5,
-    backgroundColor: "#bfbfbf"
-  },
-  mark: {
-    backgroundColor: "#bfbfbf",
-    height: 8,
-    width: 1,
-    marginTop: -3
-  },
-  markActive: {
-    opacity: 1,
-    backgroundColor: "currentColor"
-  }
-})(Slider);
+  })(Slider);  
 
-const PrettoSlider = withStyles({
-  root: {
-    color: "#dcdcdc",
-    height: 8
-  },
-  thumb: {
-    height: 24,
-    width: 24,
-    backgroundColor: "#fff",
-    border: "2px solid currentColor",
-    marginTop: -8,
-    marginLeft: -12,
-    "&:focus, &:hover, &$active": {
-      boxShadow: "inherit"
-    }
-  },
-  active: {},
-  valueLabel: {
-    left: "calc(-50% + 4px)"
-  },
-  track: {
-    height: 8,
-    borderRadius: 4
-  },
-  rail: {
-    height: 3,
-    opacity:1,
-    borderRadius: 3,
-    color: "d8d8d8"
-  }
-})(Slider);
-
-const AirbnbSlider = withStyles({
+const TimerangeSlider = withStyles({
   root: {
     color: "#dcdcdc",
     height: 3,
     padding: "8px 0"
   },
   thumb: {
-    height: 10,
-    width: 10,
-    backgroundColor: "#fff",
-    border: "1px solid currentColor",
-    marginTop: -3,
-    marginLeft: -3,
+    height: 24,
+    width: 0.1,
+    backgroundColor: "#717171",
+    border: "1px solid #717171",
+    marginTop: -9,
+    marginLeft: 0,
     boxShadow: "#ebebeb 0 2px 2px",
     "&:focus, &:hover, &$active": {
       boxShadow: "#ccc 0 2px 3px 1px"
@@ -211,65 +181,62 @@ const AirbnbSlider = withStyles({
     }
   },
   active: {},
+  valueLabel: {
+    left: "calc(+50% - 10px)",
+    top: -16,
+    "& *": {
+      background: "transparent",
+      color: "#fff",
+      backgroundColor: "#000"
+    }
+  },
   track: {
     height: 8,
-    borderRadius: 3
+    opacity: 1,
+    borderRadius: 5,
+    color: "#a9a9a9"
   },
   rail: {
-    height:3,
+    height:8,
     opacity: 1,
-    borderRadius: 3
+    borderRadius: 5
+  },
+  mark: {
+    backgroundColor: "#bfbfbf",
+    height: 8,
+    width: 1,
+    marginTop: 0,
+    color: "bfbfbf"
   }
 })(Slider);
-
-function valuetext(value) {
-    return `${value}`;
-  }
 
 export default function CustomizedSlider() {
   const classes = useStyles();
 
   return (
+      //if this.state.vtype => range | line | none
     <div className={classes.root}>
-      <Typography gutterBottom>iOS</Typography>
-      <IOSSlider
-        aria-label="ios slider"
-        defaultValue={60}
-        marks={marks}
-        valueLabelDisplay="on"
-      />
       <div className={classes.margin} />
-      <Typography gutterBottom>pretto.fr</Typography>
-      <PrettoSlider
-        valueLabelDisplay="on"
-        aria-label="pretto slider"
-        aria-labelledby="discrete-slider"
-        marks
-        step={1}
-        min={2000}
-        max={2018}
-        defaultValue={2018}
-      />
-      <div className={classes.margin} />
-      <Typography gutterBottom>Tooltip value label</Typography>
-      <Slider
+      <TimerangeSlider
         ValueLabelComponent={ValueLabelComponent}
-        aria-label="custom thumb label"
-        defaultValue={20}
-      />
-      <div className={classes.margin} />
-      <Typography gutterBottom>Airbnb</Typography>
-      <AirbnbSlider
-        getAriaValueText={(index) =>
-          index%2 === 0 ? `${index}` : ''
-        }
         marks={marks}
         step={1}
         min={2000}
         max={2018}
         aria-labelledby="discrete-slider-custom"
-        valueLabelDisplay="on"
+        valueLabelDisplay="auto"
         defaultValue={[2016, 2018]}
+      />
+      <div className={classes.margin} />
+      <TimelineSlider
+        ValueLabelComponent={ValueLabelComponent}
+        marks={marks}
+        step={1}
+        min={2000}
+        max={2018}
+        aria-labelledby="discrete-slider-custom"
+        valueLabelDisplay="auto"
+        defaultValue={2018}
       />
     </div>
   );
