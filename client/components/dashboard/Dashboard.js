97e03335e767d59a2fb1c848915695abecc57d96
Arifin Rais <44962398+arifinrais@users.noreply.github.com>
2021-06-16 10:41:06 +0700

    [REFACTOR]

--- client/components/dashboard/Dashboard.js
+++ client/components/dashboard/Dashboard.js
@@ -1,33 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Container, Row, Col} from 'react-bootstrap';
 import Visualization from './Visualization';
 import Slider from './Slider.js';
-import AkinPanel from './Panel';
+import Panel from './Panel';
 
-const AkinDashboard = props => {
-    return(
-        <Container>
-            <Row>
-                <Col xs="8" sm="8" md="8" lg="8">
-                    <Row>
-                        <h3>Paten apa saja yang dihasilkan Jawa Barat pada tahun 2018?</h3>
-                    </Row>
-                    <Row>
-                        <Visualization />
-                    </Row>
-                    <Row>
-                        <h2>CATEGORY</h2>
-                    </Row>
-                    <Row>
-                        <Slider />
-                    </Row>
-                </Col>
-                <Col xs="4" sm="4" md="4" lg="4">
-                    <AkinPanel />
-                </Col>
-            </Row>
-        </Container>
-    )
+class Dashboard extends Component {
+    render(){
+        return(
+            <Container>
+                <Row>
+                    <Col xs="8" sm="8" md="8" lg="8">
+                        <Row>
+                            <h3>Paten apa saja yang dihasilkan Jawa Barat pada tahun 2018?</h3>
+                        </Row>
+                        <Row>
+                            <Visualization />
+                        </Row>
+                        <Row>
+                            <h2>CATEGORY</h2>
+                        </Row>
+                        <Row>
+                            <Slider />
+                        </Row>
+                    </Col>
+                    <Col xs="4" sm="4" md="4" lg="4">
+                        <Panel />
+                    </Col>
+                </Row>
+            </Container>
+        )
+    }
 }
 
-export default AkinDashboard;
+export default Dashboard;
