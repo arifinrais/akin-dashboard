9cc6707fdab2029efaa47406c3fa321f1be29d87
Arifin Rais <44962398+arifinrais@users.noreply.github.com>
2021-07-02 09:47:24 +0700

    [REVAMP]

--- client/components/dashboard/Modifier.js
+++ client/components/dashboard/Modifier.js
@@ -5,6 +5,8 @@ import Paper from "@material-ui/core/Paper";
 import ToggleButton from "@material-ui/lab/ToggleButton";
 import ToggleButtonGroup from "@material-ui/lab/ToggleButtonGroup";
 import Grid from "@material-ui/core/Grid";
+import Tooltip from '@material-ui/core/Tooltip';
+import files from '../../../server/providers/resourceProvider';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -46,30 +48,46 @@ export default function CustomizedToggler(props) {
           onChange={handleChange}
           aria-label="text alignment"
         >
-          <ToggleButton value="A" aria-label="Human Necessities">
-            <img src={props.modifier.includes("A")?"/res/classicon/A_.png":"/res/classicon/A.png"} height="25" alt="" />
-          </ToggleButton>
-          <ToggleButton value="B" aria-label="Operations and Transporting">
-            <img src={props.modifier.includes("B")?"/res/classicon/B_.png":"/res/classicon/B.png"} height="25" alt="" />
-          </ToggleButton>
-          <ToggleButton value="C" aria-label="Chemistry and Metallurgy">
-            <img src={props.modifier.includes("C")?"/res/classicon/C_.png":"/res/classicon/C.png"} height="25" alt="" />
-          </ToggleButton>
-          <ToggleButton value="D" aria-label="Textiles and Paper" >
-            <img src={props.modifier.includes("D")?"/res/classicon/D_.png":"/res/classicon/D.png"} width="25" alt="" />
-          </ToggleButton>
-          <ToggleButton value="E" aria-label="Fixed Construction">
-            <img src={props.modifier.includes("E")?"/res/classicon/E_.png":"/res/classicon/E.png"} height="25" alt="" />
-          </ToggleButton>
-          <ToggleButton value="F" aria-label="Mechanical Engineering">
-            <img src={props.modifier.includes("F")?"/res/classicon/F_.png":"/res/classicon/F.png"} height="25" alt="" />
-          </ToggleButton>
-          <ToggleButton value="G" aria-label="Physics">
-            <img src={props.modifier.includes("G")?"/res/classicon/G_.png":"/res/classicon/G.png"} height="25" alt="" />
-          </ToggleButton>
-          <ToggleButton value="H" aria-label="Electricity">
-            <img src={props.modifier.includes("H")?"/res/classicon/H_.png":"/res/classicon/H.png"} height="25" alt="" />
-          </ToggleButton>
+          <Tooltip title={files.PatentCode["A"]}>
+            <ToggleButton value="A" aria-label="Human Necessities">
+              <img src={props.modifier.includes("A")?"/res/classicon/A_.png":"/res/classicon/A.png"} height="25" alt="" />
+            </ToggleButton>
+          </Tooltip>
+          <Tooltip title={files.PatentCode["B"]}>
+            <ToggleButton value="B" aria-label="Operations and Transporting">
+              <img src={props.modifier.includes("B")?"/res/classicon/B_.png":"/res/classicon/B.png"} height="25" alt="" />
+            </ToggleButton>
+          </Tooltip>
+          <Tooltip title={files.PatentCode["C"]}>
+            <ToggleButton value="C" aria-label="Chemistry and Metallurgy">
+              <img src={props.modifier.includes("C")?"/res/classicon/C_.png":"/res/classicon/C.png"} height="25" alt="" />
+            </ToggleButton>
+          </Tooltip>
+          <Tooltip title={files.PatentCode["D"]}>
+            <ToggleButton value="D" aria-label="Textiles and Paper" >
+              <img src={props.modifier.includes("D")?"/res/classicon/D_.png":"/res/classicon/D.png"} width="25" alt="" />
+            </ToggleButton>
+          </Tooltip>
+          <Tooltip title={files.PatentCode["E"]}>
+            <ToggleButton value="E" aria-label="Fixed Construction">
+              <img src={props.modifier.includes("E")?"/res/classicon/E_.png":"/res/classicon/E.png"} height="25" alt="" />
+            </ToggleButton>
+          </Tooltip>
+          <Tooltip title={files.PatentCode["F"]}>
+            <ToggleButton value="F" aria-label="Mechanical Engineering">
+              <img src={props.modifier.includes("F")?"/res/classicon/F_.png":"/res/classicon/F.png"} height="25" alt="" />
+            </ToggleButton>
+          </Tooltip>
+          <Tooltip title={files.PatentCode["G"]}>
+            <ToggleButton value="G" aria-label="Physics">
+              <img src={props.modifier.includes("G")?"/res/classicon/G_.png":"/res/classicon/G.png"} height="25" alt="" />
+            </ToggleButton>
+          </Tooltip>
+          <Tooltip title={files.PatentCode["H"]}>
+            <ToggleButton value="H" aria-label="Electricity">
+              <img src={props.modifier.includes("H")?"/res/classicon/H_.png":"/res/classicon/H.png"} height="25" alt="" />
+            </ToggleButton>
+          </Tooltip>
         </StyledToggleButtonGroup>
       </Paper>
     </Grid>
