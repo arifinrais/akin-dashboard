00bf7ac2184550b32ccfaffde95b6d6aa48ed44a
Arifin Rais <44962398+arifinrais@users.noreply.github.com>
2021-08-26 14:13:41 +0700

    Update Resource Provider

--- server/providers/resourceProvider.js
+++ server/providers/resourceProvider.js
@@ -1,9 +1,14 @@
 const main = {};
 
+main.IslandCode = require("../res/stdIsld.json");
+main.DevMainCode = require("../res/stdDevm.json");
+main.DevEconCode = require("../res/stdDeve.json");
 main.ProvinceCode = require("../res/stdProv.json");
 main.CityCode = require("../res/stdCity.json");
 main.PatentCode = require("../res/stdIpc.json");
+main.TrademarkCode = require("../res/stdNcl.json");
+main.PublicationCode = require("../res/stdKri.json");
 main.ColorCode = require("../res/stdColor.json");
-main.RegionCode = require("../res/stdReg.json");
+
 
 module.exports = main;
